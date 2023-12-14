145e5e662763de54a952247e248eefc6f0860416
ksinji <akstp1717@naver.com>
2023-12-15 03:40:36 +0900

    Connect 수정

--- Akobot/src/main/resources/static/js/app.js
+++ Akobot/src/main/resources/static/js/app.js
@@ -13,6 +13,12 @@ function setConnected(connected) {
 }
 
 function connect() {
+
+    if (stompClient !== null && stompClient.connected) {
+        // 이미 연결된 상태라면 연결 시도를 하지 않음
+        return;
+    }
+
     var socket = new SockJS('/ws');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
@@ -35,8 +41,8 @@ function disconnect() {
 function sendName() {
 
     reply = "<div class=\"bubble reply reply-freeform say\">"
-            + "<span class=\"bubble-content\">" + $("#ask").val() + "</span>"
-            + "</div>"
+        + "<span class=\"bubble-content\">" + $("#ask").val() + "</span>"
+        + "</div>"
     $("#greetings").append(reply);
 
     stompClient.send("/app/question", {}, JSON.stringify({'ask': $("#ask").val()}));
@@ -45,23 +51,25 @@ function sendName() {
 function showGreeting(messages) {
 
     for(i = 0; i < messages.length; i++){
-            for(j = 0; j < messages[i].says.length; j++){
-                if(messages[i].says[j] === null) continue;
-                if(messages[i].says[j] === "\"\"") continue;
-                if(messages[i].says[j] === "") continue;
+        for(j = 0; j < messages[i].says.length; j++){
+            if(messages[i].says[j] === null) continue;
+            if(messages[i].says[j] === "\"\"") continue;
+            if(messages[i].says[j] === "") continue;
 
-                messageToken = messages[i].says[j].split('\\n')
-                for(k = 0; k < messageToken.length; k++){
-                    str = "<div class=\"bubble say\">"
-                        + "<span class=\"bubble-content\">" + messageToken[k] + "</span>"
-                        + "</div>";
-                    $("#greetings").append(str);
-                }
+            messageToken = messages[i].says[j].split('\\n')
+            for(k = 0; k < messageToken.length; k++){
+                str = "<div class=\"bubble say\">"
+                    + "<span class=\"bubble-content\">" + messageToken[k] + "</span>"
+                    + "</div>";
+                $("#greetings").append(str);
             }
         }
+    }
 }
 
 $(function () {
+    connect(); //웹페이지에 접속하면 기본으로 연결 상태가 되도록 함
+
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
